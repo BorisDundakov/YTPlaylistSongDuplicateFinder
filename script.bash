f2e00c90cd063a95e8fa1d0667030f5e0cc56aa2
BorisDundakov <boris.stefan.de@gmail.com>
2023-01-26 12:55:04 +0200

    feat: age restricted video access

--- script.bash
+++ script.bash
@@ -1,14 +1,16 @@
 #!/bin/bash
-# Checks your youtube playlist for duplicate songs. If all dupolicate entries are stored in a duplicates.txt file
+# Checks your Youtube playlist for duplicate songs. If all duplicate entries are stored in a duplicates.txt file
 # You can search for the song in your playlist and delete the duplicate entry inf you wish to do so
 
-# Input playlist url (must be a pulbic playlist)
+# Input playlist url (must be a public playlist)
 #TODO: Fix (improve) this later!
 #printf "\nType playlist url and press enter: "
 #read playlist
 
 playlist=$1
-	
+username=$2
+pass=$3
+cookies=$4
 if [ "$playlist" = 'X' ]
 then
 		exit 0
@@ -17,9 +19,9 @@ fi
 printf "\nChecking for duplicate songs. This might take a while if the playlist is large...\n"
 
 # store all songs in songs.txt file		
-youtube-dl --get-filename -o "%(playlist_title)s - %(title)s - %(id)s" "$playlist" -i > songs.txt
+youtube-dl -u "$username" --password "$pass" --get-filename --cookies "$cookies"-o "%(playlist_title)s - %(title)s - %(id)s" "$playlist" -i  > songs.txt
 
-echo "All songs checked! If any dupliates exist, they will be stored inside 'duplicates.txt'."
+echo "All songs checked! If any duplicates exist, they will be stored inside 'duplicates.txt'."
 
 # copy all the duplicates to a duplicates file
 sort songs.txt | uniq -d > duplicates.txt
