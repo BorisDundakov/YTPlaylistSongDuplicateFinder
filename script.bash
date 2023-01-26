c5cc9202843658c0af130eb8997363120879b7a3
BorisDundakov <boris.stefan.de@gmail.com>
2023-01-26 19:12:47 +0200

    switch to youtube-dlp

--- script.bash
+++ script.bash
@@ -8,9 +8,6 @@
 #read playlist
 
 playlist=$1
-username=$2
-password=$3
-cookies='cookies.txt'
 
 if [ "$playlist" = 'X' ]
 then
@@ -20,12 +17,12 @@ fi
 printf "\nChecking for duplicate songs. This might take a while if the playlist is large...\n"
 
 # store all songs in songs.txt file
-youtube-dl  --username "$username" --password "$password" --get-filename -o "%(playlist_title)s - %(title)s - %(id)s" "$playlist" -i  > songs.txt
-
-echo "All songs checked! If any duplicates exist, they will be stored inside 'duplicates.txt'."
+yt-dlp --print filename "$playlist" -i > songs.txt
 
 # copy all the duplicates to a duplicates file
 sort songs.txt | uniq -d > duplicates.txt
+
+echo "All songs checked! If any duplicates exist, they will be stored inside 'duplicates.txt'."
 
 # remove the file as we no longer need it
 rm songs.txt
