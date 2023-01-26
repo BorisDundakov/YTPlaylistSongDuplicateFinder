ddcd30c42fda49dfa78ff010e2af724a680125f3
BorisDundakov <boris.stefan.de@gmail.com>
2023-01-26 13:17:53 +0200

    feat: age restriction workaround

--- script.bash
+++ script.bash
@@ -19,7 +19,7 @@ fi
 printf "\nChecking for duplicate songs. This might take a while if the playlist is large...\n"
 
 # store all songs in songs.txt file		
-youtube-dl -u "$username" --password "$pass" --get-filename --cookies "$cookies"-o "%(playlist_title)s - %(title)s - %(id)s" "$playlist" -i  > songs.txt
+youtube-dl --cookies cookies.txt --get-filename -o "%(playlist_title)s - %(title)s - %(id)s" "$playlist" -i  > songs.txt
 
 echo "All songs checked! If any duplicates exist, they will be stored inside 'duplicates.txt'."
 
