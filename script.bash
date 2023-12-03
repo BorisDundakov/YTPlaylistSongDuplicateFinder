cad0e36967186ea738de56c4cdc6c853511ed8e6
Boris Dundakov <71731579+BorisDundakov@users.noreply.github.com>
2023-12-03 13:15:59 +0200

    Update script.bash

--- script.bash
+++ script.bash
@@ -1,17 +1,18 @@
 #!/bin/bash
+
 # Checks your YouTube playlist for duplicate songs. If all duplicate entries are stored in a duplicates.txt file
-# You can search for the song in your playlist and delete the duplicate entry inf you wish to do so
+# You can search for the song in your playlist and delete the duplicate entry if you wish to do so
 
 # Input playlist url (must be a public playlist)
-#TODO: Fix (improve) this later!
-#printf "\nType playlist url and press enter: "
-#read playlist
 
+printf "\nType playlist url and press enter: "
+
+#read playlist
 playlist=$1
 
 if [ "$playlist" = 'X' ]
 then
-		exit 0
+        exit 0
 fi
 
 printf "\nChecking for duplicate songs. This might take a while if the playlist is large...\n"
@@ -26,4 +27,3 @@ echo "All songs checked! If any duplicates exist, they will be stored inside 'du
 
 # remove the file as we no longer need it
 rm songs.txt
-
